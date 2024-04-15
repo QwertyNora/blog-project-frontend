bfbfffc32c1a58b1a038385b9c6e73e800e73a56
Nora Silfver <98233189+QwertyNora@users.noreply.github.com>
2024-04-15 22:04:06 +0200

    create react app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Min blogg! </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
